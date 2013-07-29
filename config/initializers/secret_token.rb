f98d7662f3d574d1240b10c8e6dedcb6786d820d
Mitch Winkel-Davis <mitch@mitchwd.com>
2013-07-29 16:38:14 +0930

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smswall::Application.config.secret_key_base = '2f86cc5644286795341b8fe2f738fb198470aa1a2b91b14a50beb39849f8efa8a634db09c05bd4795f93ee1b4cf4d2fe7cb7a304e0dc2351d0d02769a3092811'
