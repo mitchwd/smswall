3a2a61eecabd162dfba589fa67614f41d1fc55d2
Mitch Winkel-Davis <mitch@mitchwd.com>
2013-08-25 12:18:01 +0930

    Created basic setup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Smswall::Application.routes.draw do
   get "static/index"
   get "static/about"
+  get "static/setup"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/request/static_spec.rb
+++ spec/request/static_spec.rb
@@ -2,19 +2,32 @@ require 'spec_helper'
 
 describe "Static" do
   describe "GET /" do
-
     it "Render success" do
       visit '/'
       expect(page.status_code).to be(200)
     end
-
   end
 
   describe "GET /about" do
-
     it "Render success" do
       visit '/static/about'
       expect(page.status_code).to be(200)
+    end
+  end
+
+  describe "GET /setup" do
+    it "Render success" do
+      visit '/static/setup'
+      expect(page.status_code).to be(200)
+    end
+
+    it "shows a link to a provider" do
+      visit '/static/setup'
+      expect(page).to have_content("www.nexmo.com")
+    end
+
+    it "shows the callback url" do
+      pending "Create controller first."
     end
   end
 end
